f00f559a636ad4162eb436b383c83a44993c9d44
luckyanand15 <luckyanand.741@gmail.com>
2024-11-05 03:30:46 +0530

    Navbar responsive

--- medify/src/components/NavBar/NavBar.jsx
+++ medify/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Styles from "./NavBar.module.css";
-import { Box, Button, Container, Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Stack,
+  useMediaQuery,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
 const NavBar = () => {
+  const mobileSize = useMediaQuery("(max-width:900px)");
+  const [isMenuOpen, setMenuOpen] = useState(false);
   return (
-    <div>
+    <Box>
       <Container maxWidth="xl">
         <Stack
           direction={"row"}
           justifyContent={"space-between"}
           alignItems={"center"}
           py={3}
-          px={10}
+          px={mobileSize ? 2:10}
         >
           <Link to="/">
             <img src={logo} alt="Medify" height={27} />
@@ -20,8 +31,11 @@ const NavBar = () => {
           <Stack
             direction={{ xs: "column", md: "row" }}
             spacing={4}
-            alignItems={{ md: "center", xs: "flex-start" }}
-            className={Styles.navBarLinks}
+            alignItems={{ xs: "flex-start", md: "center" }}
+            className={[Styles.navBarLinks, isMenuOpen && Styles.enabled]}
+            pt={{xs:12, md:1}}
+            pb={{xs:4, md: 1}}
+            px={{xs:4}}
           >
             <Link to="/search">Find Doctors</Link>
             <Link to="/search">Hospitals</Link>
@@ -32,10 +46,23 @@ const NavBar = () => {
             <Link to="/my-bookings">
               <Button variant="contained">My Bookings</Button>
             </Link>
+            {mobileSize && (
+              <IconButton
+                onClick={() => setMenuOpen(false)}
+                sx={{ position: "absolute", top: 0, right:32, color:"#ffffff" }}
+              >
+                <CloseIcon />
+              </IconButton>
+            )}
           </Stack>
+          {mobileSize && (
+              <IconButton onClick={() => setMenuOpen(true)}>
+                <MenuIcon />
+              </IconButton>
+            )}
         </Stack>
       </Container>
-    </div>
+    </Box>
   );
 };
 
